75f5b7ccac72106634e1d7685f731718c863a391
richardjamesw <rwallace053@gmail.com>
2016-10-12 10:42:32 -0400

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,13 @@
+from Observation import Observation
+import time
+import os
+
+# open file
+data_file = open("time_series.txt")
+mod_time = os.path.getmtime(data_file)
+
+# read file
+data_file =
+
+# close file
+
